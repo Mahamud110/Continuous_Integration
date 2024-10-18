5990bb8e112ba757ab0745a1a31a1a568d6deacf
Mahamud110 <mahamud110@hotmail.com>
2024-10-18 15:35:05 +0100

    function+Unittest

--- Test_Suite.py
+++ Test_Suite.py
@@ -1,1 +1,17 @@
+import unittest
+from Add import *
+
+class TestFunction(unittest.TestCase):
+    def test_addition(self):
+        result = add(3,5)
+        self.assertEqual(result,8)
+
+    def test_addition_neg(self):
+        result = add(-2,7)
+        self.assertEqual(result,5)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
 
